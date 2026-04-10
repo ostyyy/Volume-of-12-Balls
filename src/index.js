99df93f96a00db239615930bf6f450791d609153
ostyyy <ggg468432@gmail.com>
2026-04-10 19:13:09 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 
 const NUM = 12;
 const THICKNESS = 0.01;
-const R0_OUTER  = 0.25;
+const R0_OUTER = 0.25;
 
 let totalVolume = 0;
 
 for (let i = 0; i < NUM; i++) {
-    let outerR = R0_OUTER  + i * THICKNESS;
-    let innerR = outerR - THICKNESS;
+    const outerR = R0_OUTER + i * THICKNESS;
+    const innerR = outerR - THICKNESS;
 
-    let innerVolume = (4 / 3) * Math.PI * innerR ** 3;
+    const innerVolume = (4 / 3) * Math.PI * innerR ** 3;
     totalVolume += innerVolume;
 }
 
-console.log(`Total inner volume of 12 balls: ${totalVolume} m^3`);
+console.log(`Total inner volume of 12 nested balls: ${totalVolume} m^3`);
